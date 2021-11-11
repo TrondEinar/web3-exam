ab62334be3208ae2aa9d7030cb15f43a23125ccf
TrondEinar <54721335+TrondEinar@users.noreply.github.com>
2021-11-11 22:29:51 +0100

    merged trondeinar_branch into main (#1)
    * Connect mongoDb to frontend
    
    * mongoDb improvements

--- mma/src/components/athlete/AthleteItem.tsx
+++ mma/src/components/athlete/AthleteItem.tsx
@@ -1,1 +1,15 @@
 import { FC } from "react";
+import { IAthlete } from "../../interfaces/IAthlete";
+
+const AthleteItem: FC<IAthlete> = ({ id, name, image }) => {
+  return (
+    <article>
+      <h3>
+        {name} {id}
+      </h3>
+      <img src={`https://localhost:5001/images/${image}`} alt={name} />
+    </article>
+  );
+};
+
+export default AthleteItem;
--- mma/src/services/athleteService.ts
+++ mma/src/services/athleteService.ts
@@ -1,1 +1,15 @@
 import axios from "axios";
+import { IAthlete } from "../interfaces/IAthlete";
+
+export const athleteService = (function () {
+  const UrlToAthleteController = "https://localhost:5001/MmaAthlete";
+
+  const getAllAthletes = async () => {
+    const result = await axios.get(UrlToAthleteController);
+    return result.data as IAthlete[];
+  };
+
+  return {
+    getAllAthletes,
+  };
+})();
