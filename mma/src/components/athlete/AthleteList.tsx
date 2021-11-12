d5addb0b64ec253047b3bacde129416e74596b57
TrondEinar <trond-einar98@hotmail.no>
2021-11-12 15:38:10 +0100

    Bootstrap Card

--- mma/src/components/athlete/AthleteList.tsx
+++ mma/src/components/athlete/AthleteList.tsx
@@ -2,6 +2,7 @@ import { FC, useState, useEffect } from "react";
 import { athleteService } from "../../services/athleteService";
 import { IAthlete } from "../../interfaces/IAthlete";
 import AthleteItem from "./AthleteItem";
+import { Container, Col, Row } from "react-bootstrap";
 
 const AthleteList: FC = () => {
   const [athletes, setAthletes] = useState<IAthlete[]>();
@@ -18,12 +19,20 @@ const AthleteList: FC = () => {
   const createAthleteList = () => {
     return athletes?.map((athlete: IAthlete, key: number) => {
       return (
-        <AthleteItem key={key} name={athlete.name} image={athlete.image} />
+        <Col key={key}>
+          <AthleteItem name={athlete.name} image={athlete.image} />
+        </Col>
       );
     });
   };
 
-  return <section>{createAthleteList()}</section>;
+  return (
+    <Container>
+      <Row xs={1} md={3} className="g-4">
+        {createAthleteList()}
+      </Row>
+    </Container>
+  );
 };
 
 export default AthleteList;
