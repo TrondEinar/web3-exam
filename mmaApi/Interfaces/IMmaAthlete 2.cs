0c92bcde4c530839fd3cd5bbdd9d5178a923cdc7
TrondEinar <trond-einar98@hotmail.no>
2021-11-17 21:32:01 +0100

    removed duplicate class

--- mmaApi/Interfaces/IMmaAthlete 2.cs
+++
@@ -1,9 +0,0 @@
-namespace MmaAthletesApi.Interfaces
-{
-    public interface IMmaAthlete
-    {
-        string Id {get; set;}
-        string Name {get; set;}
-        string Image {get; set;}
-    }
-}
